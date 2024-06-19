402fc23a45b85e9687bfd8d24bc0fb45d30cdfd3
parthmern <parthmern@gmail.com>
2024-06-18 22:55:13 -0400

    initiate client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center gap-4 flex-col min-h-screen">
+      intiate project
+    </main>
+  );
+}
+
+export default App;
